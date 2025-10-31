f026fdb4d7e530f3438d28f5f9c5d10e785b93b8
Jamey Verlinden <r1042086@student.thomasmore.be>
2025-10-31 16:35:21 +0100

    shoppingnode functionality added for shoppinghistory page

--- routes/shoppingnode.js
+++ routes/shoppingnode.js
@@ -12,7 +12,7 @@ const { startOfWeek, startOfMonth, startOfYear, format } = require('date-fns');
 //import { DateRange } from "easy-date-range";
 
 // --------------------------------------------
-// [GET] shoppingnode 
+// [GET] shoppingnode (SHOPPINGHISTORY PAGE)
 // return all data of shoppingnode + Total amount spend + Amount of shopping trips + most visited store
 // WHERE datefilter (this week, this month, this year)
 // --------------------------------------------
@@ -24,9 +24,34 @@ router.get('/', async(req, res) => {
   let startdate = format(findstartdate(timefilter), "yyyy-MM-dd");
   let enddate = format(new Date(), "yyyy-MM-dd");
   
-  let shoppingnodes = await prisma.$queryRaw`SELECT * FROM shoppingnode WHERE shoppingdate BETWEEN ${startdate} AND ${enddate}`;
+  let shoppingnodes = await prisma.$queryRaw`SELECT * FROM shoppingnode WHERE shoppingdate BETWEEN ${startdate} AND ${enddate}
+  AND shoppingnode.futurepurchase = 0`;
 
-  res.json(shoppingnodes);
+  // Find total amount spend (from receipts linked to shoppingnode)
+  let totalspend = await prisma.$queryRaw`SELECT SUM(price) FROM receipt as RE
+  INNER JOIN shoppingnode as SN on RE.receiptID = SN.receiptID
+  WHERE SN.shoppingdate BETWEEN ${startdate} AND ${enddate} AND SN.usersID = 3 
+  AND SN.futurepurchase = false`;
+
+  // Count # shopping trips
+  let countshoppingtrips = await prisma.$queryRaw`SELECT CAST(COUNT(*) AS CHAR) FROM shoppingnode 
+  WHERE shoppingdate BETWEEN ${startdate} AND ${enddate}
+  AND shoppingnode.futurepurchase = false`;
+  
+  // res.json({total: Number(countshoppingtrips[0]["COUNT(*)"])}); Yeah because it's a bigint for some reason
+  // Most visted store
+  let mostvisitedstore = await prisma.$queryRaw`SELECT RE.storeID, ST.storename, CAST(Count(RE.storeID) AS CHAR) as VisitCount FROM receipt as RE
+  INNER JOIN shoppingnode as SN on RE.receiptID = SN.receiptID
+  INNER JOIN store as ST on RE.storeID = ST.storeID
+  WHERE SN.shoppingdate BETWEEN ${startdate} AND ${enddate} 
+  AND SN.usersID = 3 AND SN.futurepurchase = false GROUP BY storeID ORDER BY VisitCount desc LIMIT 1;`;
+
+  res.json({
+    nodes: shoppingnodes,
+    total: totalspend,
+    count: countshoppingtrips,
+    store: mostvisitedstore
+  });
 })
 
 function findstartdate(dmy){
@@ -43,11 +68,6 @@ function findstartdate(dmy){
     const yearStart = startOfYear(today);
     return yearStart;
   }
-}
-
-function localDate(year, month, day) {
-  // month is 0-indexed
-  return new Date(year, month, day, 0, 0, 0, 0);
 }
 
 module.exports = router;
