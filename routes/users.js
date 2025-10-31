dc9fe008cdedb70949fa456068058c26ab7b0135
Jamey Verlinden <r1042086@student.thomasmore.be>
2025-10-31 12:24:36 +0100

    Register new account function made

--- routes/users.js
+++ routes/users.js
@@ -55,13 +55,71 @@ router.get('/', async(req, res) => { //Async = Wacht op antwoord op database
 // --------------------------------------------
 // [POST] users (REGISTER PAGE)
 // return userID
-// Gmail notification. Send welcome email
+// Gmail notification. Send welcome email (+ validate gmail address I suppose?)
 // --------------------------------------------
 
 router.post('/', async(req, res) => { 
   const usersName = req.body.UN;
   const usersGmail = req.body.UG;
   const usersPassword = req.body.UP;
+
+  let pass = passwordCheck(usersPassword);
+  if(pass == "correct"){
+    const checkGmail = await prisma.users.findMany({
+      where:{
+        gmail: usersGmail
+      }
+    });
+
+    if(checkGmail.Length === 0){
+      res.json({
+        "error": "gmail address is already taken. Please enter valid gmail address"
+      });
+    }
+    else{
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(usersPassword, salt);
+
+      const insertuser = await prisma.users.create({
+        data:{
+          username: usersName,
+          userpassword: hashedPassword,
+          gmail: usersGmail,
+          budget: null,
+          permission: 1
+        }
+      });
+      res.json(insertuser);
+    }
+
+  }
+  else{
+    res.json({
+      "error": pass
+    })
+  }
 });
+
+function passwordCheck(password){
+  const digitpattern = /[0-9]/g;
+  const upperpattern = /[A-Z]/g;
+  const specialpattern = /[^\w\s]/g;
+  
+  //Check length
+  if(password.length < 12){
+    return "Password must include at least 12 characters"
+  }
+  else if(password.match(digitpattern).length < 4){
+    return "Password must include at least 4 digits"
+  }
+  else if(password.match(upperpattern).length < 4){
+    return "Password must include at least 4 uppercase characters"
+  }
+  else if(password.match(specialpattern).length < 2){
+    return "Password must include at least 2 special characters"
+  }
+
+  return "correct"
+}
 
 module.exports = router;
